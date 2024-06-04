cbe9e6501719397ab5641a31e9fcc356036ba295
JaredTherriault <noirjt@live.com>
2024-06-04 15:44:37 -0700

    Disable wheelseek by default

--- web/js/common/VideoOptions.js
+++ web/js/common/VideoOptions.js
@@ -1,7 +1,7 @@
 import { setVideoPlaybackRate, setVideoVolume } from "./VideoControl.js";
 
 export class options_VideoPlayback { 
-    autoplay = false; loop = true; controls = true; muted = true; useWheelSeek = true; invertWheelSeek = true; defaultVolume = 50; defaultPlaybackRate = 1.00;
+    autoplay = false; loop = true; controls = true; muted = true; useWheelSeek = false; invertWheelSeek = true; defaultVolume = 50; defaultPlaybackRate = 1.00;
 }; 
 
 export class info_VideoPlaybackOptions {
