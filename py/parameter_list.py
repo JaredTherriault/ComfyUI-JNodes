7f54444a3ab418399f18a01220a30bbf3dae4739
JaredTherriault <noirjt@live.com>
2024-07-10 17:53:16 -0700

    Clarify logging, allow ":" in parameter values

--- py/parameter_list.py
+++ py/parameter_list.py
@@ -18,22 +18,25 @@ RETURN_TYPES = ["auto", "string"]
 PARAMETER_LIST_HINT_TEXT = "<jnodes_param:my_string_variable:string>\n<jnodes_param:my_number_variable: 0.5>"
 
 def parse_parameter_list(parameter_list, parsing_key, break_on_finding_term : str = None):
-    regex = r'<' + re.escape(parsing_key) + r':(.+?)>'
-    
-    matches = re.findall(regex, parameter_list)
+
     key_value_pairs = {}
-    
-    for match in matches:
-        pair = match.split(':')
-        key = pair[0].strip()
-        if key not in key_value_pairs: # take the first value, do not overwrite
-            if len(pair) > 1:
-                value = pair[1].strip()
-            else: # If no parameter specified, assume it's a boolean
-                value = True;
-            key_value_pairs[key] = value
-            if break_on_finding_term != None and break_on_finding_term == key: # early out if we found specified term
-                break
+
+    if parameter_list and parsing_key:
+        regex = r'<' + re.escape(parsing_key) + r':(.+?)>'
+        
+        matches = re.findall(regex, parameter_list)
+        
+        for match in matches:
+            split = match.split(':')
+            key = split.pop(0).strip()
+            if key not in key_value_pairs: # take the first value, do not overwrite
+                if len(split) > 0:
+                    value = ":".join(split).strip() # For parameters that may have had a literal ":" in them, like "(dog:1.2)"
+                else: # If no parameter specified, assume it's a boolean
+                    value = True
+                key_value_pairs[key] = value
+                if break_on_finding_term != None and break_on_finding_term == key: # early out if we found specified term
+                    break
 
     return key_value_pairs
 
@@ -64,12 +67,12 @@ def auto_convert_output(output, explicit_return_type = None):
 
     return output
 
-def return_paramter_from_list(list, parameter_name, parameter_default, explicit_return_type = None):
-    if parameter_name in list:
-        return auto_convert_output(list[parameter_name], explicit_return_type)
+def return_paramter_from_list(parsed_list, parameter_name, parameter_default = "0", explicit_return_type = None):
+    if parsed_list and parameter_name in parsed_list:
+        return auto_convert_output(parsed_list[parameter_name], explicit_return_type)
     else:
-        logger.warning(f"Did not find parameter_name ({parameter_name}) in parameter_list. Using parameter_default (or 0).")
-        return auto_convert_output(parameter_default if parameter_default is not None else "0", explicit_return_type)
+        logger.info(f"Did not find parameter_name '{parameter_name}' in parameter_list. Using '{parameter_default}'.")
+        return auto_convert_output(parameter_default, explicit_return_type)
     
 def add_param_to_png_info(extra_pnginfo, parameter_name, param):
     if extra_pnginfo:
@@ -168,9 +171,9 @@ class GetParameterFromList:
             return_type = None, add_to_png_info = True, parameter_default = None, extra_pnginfo = None):
         
         # early out when finding first instance of parameter_name
-        list = parse_parameter_list(parameter_list, parsing_key, parameter_name) 
+        parsed_list = parse_parameter_list(parameter_list, parsing_key, parameter_name) 
         
-        param = return_paramter_from_list(list, parameter_name, parameter_default, return_type)
+        param = return_paramter_from_list(parsed_list, parameter_name, parameter_default, return_type)
         
         if add_to_png_info:
             add_param_to_png_info(extra_pnginfo, parameter_name, param)
