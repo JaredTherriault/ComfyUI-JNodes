5469e546231dec1e51cfe12ceac36c565c8f0a58
JaredTherriault <noirjt@live.com>
2025-01-31 13:12:47 -0800

    Attempt to ensure parameter nodes run every time without causing connected nodes to have to run every time

--- py/parameter_list.py
+++ py/parameter_list.py
@@ -7,13 +7,17 @@ from .utils import any, AnyType
 import json
 import re
 
+import hashlib
+
 import comfy.sd
 
 from typing import Dict, List
 
 JNODES_PARAMETER_LIST = {}
 
-RETURN_TYPES = ["auto", "string"]
+JNODES_PARAMETER_LAST_CACHE = {}
+
+RETURN_TYPES_LIST = ["auto", "string"]
 
 PARAMETER_LIST_HINT_TEXT = "<params:my_string_variable:string>\n<params:my_number_variable: 0.5>"
 
@@ -71,12 +75,12 @@ def return_paramter_from_list(parsed_list, parameter_name, parameter_default = "
     if parsed_list and parameter_name in parsed_list:
         return auto_convert_output(parsed_list[parameter_name], explicit_return_type)
     else:
-        logger.info(f"Did not find parameter_name '{parameter_name}' in parameter_list. Using '{parameter_default}'.")
+        logger.info(f"Did not find parameter_name '{parameter_name}' in parameter_list. Using '{parameter_default if len(str(parameter_default)) < 5 else "given default"}'.")
         return auto_convert_output(parameter_default, explicit_return_type)
     
 def add_param_to_png_info(extra_pnginfo, parameter_name, param):
     if extra_pnginfo:
-        extra_pnginfo[parameter_name] = str(param)
+        extra_pnginfo[parameter_name] = str(param).strip()
 
 class ParseParametersToGlobalList:
     @classmethod        
@@ -88,7 +92,6 @@ class ParseParametersToGlobalList:
         }
     }
 
-    OUTPUT_NODE = True
     RETURN_TYPES = ("STRING",)
     FUNCTION = "parse_parameters_to_global_list"
 
@@ -105,7 +108,7 @@ class GetParameterGlobal:
         return {
             "required": {
                 "parameter_name": ("STRING", {"multiline": False}),
-                "return_type": (RETURN_TYPES,),
+                "return_type": (RETURN_TYPES_LIST,),
                 "add_to_png_info": ("BOOLEAN",{"default": True}),
             },
             "optional": {
@@ -150,13 +153,14 @@ class GetParameterFromList:
                 "parameter_list": ("STRING", {"multiline": True, "default": PARAMETER_LIST_HINT_TEXT}),
                 "parsing_key": ("STRING", {"multiline": False, "default": "params"}),
                 "parameter_name": ("STRING", {"multiline": False}),
-                "return_type": (RETURN_TYPES,),
+                "return_type": (RETURN_TYPES_LIST,),
                 "add_to_png_info": ("BOOLEAN",{"default": True}),
             },
             "optional": {
                 "parameter_default": (any,),
             },
             "hidden": {
+                "unique_id": "UNIQUE_ID",
                 "extra_pnginfo": "EXTRA_PNGINFO",
             },
     }
@@ -165,20 +169,60 @@ class GetParameterFromList:
     FUNCTION = "get_parameter_from_list"
 
     CATEGORY = "parameter_list"
-    
-    def get_parameter_from_list(
-            self, parameter_list, parsing_key, parameter_name, 
-            return_type = None, add_to_png_info = True, parameter_default = None, extra_pnginfo = None):
-        
+
+    OUTPUT_NODE = True
+
+    @staticmethod
+    def get_param(parameter_list, parsing_key, parameter_name, return_type = None, parameter_default = None):
+
         # early out when finding first instance of parameter_name
         parsed_list = parse_parameter_list(parameter_list, parsing_key, parameter_name) 
         
         param = return_paramter_from_list(parsed_list, parameter_name, parameter_default, return_type)
+
+        return param
+    
+    def get_parameter_from_list(
+            self, parameter_list, parsing_key, parameter_name, 
+            return_type = None, add_to_png_info = True, parameter_default = None, unique_id = None, extra_pnginfo = None):
+
+        param = GetParameterFromList.get_param(parameter_list, parsing_key, parameter_name, return_type, parameter_default)
+        JNODES_PARAMETER_LAST_CACHE[unique_id] = param
         
         if add_to_png_info:
             add_param_to_png_info(extra_pnginfo, parameter_name, param)
             
         return (param,)
+    
+    @classmethod
+    def IS_CHANGED(s, parameter_list, parsing_key, parameter_name, 
+            return_type = None, add_to_png_info = True, parameter_default = None, unique_id = None, extra_pnginfo = None):
+        
+        return_value = True
+
+        default = parameter_default
+        cached = None
+        if unique_id and unique_id in JNODES_PARAMETER_LAST_CACHE and JNODES_PARAMETER_LAST_CACHE[unique_id] is not None:
+            cached = JNODES_PARAMETER_LAST_CACHE[unique_id]
+            default = cached
+        else:
+            return False
+        
+        param = None
+        try:
+            param = GetParameterFromList.get_param(parameter_list, parsing_key, parameter_name, return_type, default)
+
+            if param == cached:
+                return_value = False
+        except:
+            if cached:
+                param = cached
+                return_value = False
+
+        if param is not None and add_to_png_info:
+            add_param_to_png_info(extra_pnginfo, parameter_name, param)
+
+        return return_value
     
 NODE_CLASS_MAPPINGS = {
     
